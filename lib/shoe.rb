9b5bb1cc8f6583340474d46c3b62e3e4f3002545
Milos Popovic <milos_popovic@outlook.com>
2020-07-21 17:29:17 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,13 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+    # BRANDS.delete_if{ |e| BRANDS.count(e) > 1 }
+    BRANDS.uniq!  #Returns a new array by removing duplicate values
   end
 
   def cobble
