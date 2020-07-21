286ccd068f53cb4949b5b28a8c027bb40690836a
Milos Popovic <milos_popovic@outlook.com>
2020-07-21 17:23:42 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,8 +7,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
     BRANDS << brand
-    BRANDS.detect{ |e| BRANDS.count(e) < 2 }
-    BRANDS
+    BRANDS.delete_if{ |e| BRANDS.count(e) > 1 }
   end
 
   def cobble
